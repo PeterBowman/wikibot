73010404dca26f1312bfdda6cb8e420d2e1dcab3
Peter Bowman <bwmn.peter@gmail.com>
2015-07-08 21:18:03 +0200

    MaintenanceScript: implement time gap to delay page checking

--- wikibot/src/com/github/wikibot/tasks/eswikt/MaintenanceScript.java
+++ wikibot/src/com/github/wikibot/tasks/eswikt/MaintenanceScript.java
@@ -1,6 +1,7 @@
 package com.github.wikibot.tasks.eswikt;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -8,9 +9,7 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
 import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 import java.util.TimeZone;
 import java.util.function.BiConsumer;
@@ -38,44 +37,42 @@ public final class MaintenanceScript {
 	private static final String LOCATION = "./data/tasks.eswikt/MaintenanceScript/";
 	private static final String LAST_DATE = LOCATION + "last_date.txt";
 	private static final String PICK_DATE = LOCATION + "pick_date.txt";
-	
 	public static void main(String[] args) throws FailedLoginException, IOException, ParseException {
-		File f_last_date = new File(LAST_DATE);
-		File f_pick_date = new File(PICK_DATE);
+		String startTimestamp = extractTimestamp();
 		
-		String startTimestamp = null;
+		int gap;
 		
-		if (f_last_date.exists()) {
-			startTimestamp = IOUtils.loadFromFile(LAST_DATE, "", "UTF8")[0];
-		} else if (f_pick_date.exists()) {
-			startTimestamp = IOUtils.loadFromFile(PICK_DATE, "", "UTF8")[0];
-		} else {
-			System.out.println("No initial timestamp found, aborting...");
-			return;
+		try {
+			gap = Integer.parseInt(args[0]);
+		} catch (ArrayIndexOutOfBoundsException | NumberFormatException e) {
+			gap = 0;
 		}
 		
 		ESWikt wb = Login.retrieveSession(Domains.ESWIKT, Users.User2);
 		
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-		
 		Calendar startCal = Calendar.getInstance();
 		startCal.setTime(dateFormat.parse(startTimestamp));
 		
 		Calendar endCal = wb.makeCalendar();
+		Calendar gapCal = (Calendar) endCal.clone();
 		
-		if (endCal.before(startCal)) {
+		if (gap > 0) {
+			gapCal.add(Calendar.HOUR, -gap);
+		}
+		if (gapCal.before(startCal)) {
 			return;
 		}
 		
 		int rcoptions = Wikibot.HIDE_REDIRECT | Wikibot.HIDE_BOT;
 		int rctypes = Wikibot.RC_NEW | Wikibot.RC_EDIT;
 		
-		List<String> titles = new ArrayList<String>(Stream
+		List<String> titles = Stream
 			.of(wb.recentChanges(startCal, endCal, rcoptions, rctypes, false, Wikibot.MAIN_NAMESPACE))
-			.map(Revision::getPage)
-			.collect(Collectors.toCollection(LinkedHashSet::new)));
+			.collect(new RevisionCollector(gapCal));
 		
 		PageContainer[] pages = Stream.of(wb.getContentOfPages(titles.toArray(new String[titles.size()])))
+			// TODO: implement a Comparator in PageContainer so this is not necessary anymore
 			.sorted((pc1, pc2) -> Integer.compare(titles.indexOf(pc1.getTitle()), titles.indexOf(pc2.getTitle())))
 			.toArray(PageContainer[]::new);
 		
@@ -102,63 +99,67 @@ public final class MaintenanceScript {
 		}
 		
 		dateFormat.setTimeZone(TimeZone.getTimeZone("GMT"));
-		IOUtils.writeToFile(dateFormat.format(endCal.getTime()), LAST_DATE);
+		IOUtils.writeToFile(dateFormat.format(gapCal.getTime()), LAST_DATE);
 		
 		Login.saveSession(wb);
 	}
+	
+	private static String extractTimestamp() throws FileNotFoundException {
+		File f_last_date = new File(LAST_DATE);
+		File f_pick_date = new File(PICK_DATE);
+		
+		String startTimestamp;
+		
+		if (f_last_date.exists()) {
+			startTimestamp = IOUtils.loadFromFile(LAST_DATE, "", "UTF8")[0];
+		} else if (f_pick_date.exists()) {
+			startTimestamp = IOUtils.loadFromFile(PICK_DATE, "", "UTF8")[0];
+		} else {
+			throw new UnsupportedOperationException("No timestamp file found.");
+		}
+		
+		if (startTimestamp.isEmpty()) {
+			throw new UnsupportedOperationException("No initial timestamp found.");
+		}
+		
+		return startTimestamp;
+	}
 }
 
-// https://weblogs.java.net/blog/kocko/archive/2014/12/19/java8-how-implement-custom-collector
-// http://www.nurkiewicz.com/2014/07/introduction-to-writing-custom.html
+class RevisionCollector implements Collector<Revision, LinkedHashMap<String, Revision>, List<String>> {
+	// https://weblogs.java.net/blog/kocko/archive/2014/12/19/java8-how-implement-custom-collector
+	// http://www.nurkiewicz.com/2014/07/introduction-to-writing-custom.html
+	
+	private Calendar cal;
 
-class PageContainerCollector implements Collector<Revision, LinkedHashMap<String, Long>, PageContainer[]> {
-	private Wikibot wb;
-
-	public PageContainerCollector(Wikibot wb) {
-		this.wb = wb;
+	public RevisionCollector(Calendar cal) {
+		this.cal = cal;
 	}
 	
 	@Override
-	public Supplier<LinkedHashMap<String, Long>> supplier() {
+	public Supplier<LinkedHashMap<String, Revision>> supplier() {
 		return LinkedHashMap::new;
 	}
 
 	@Override
-	public BiConsumer<LinkedHashMap<String, Long>, Revision> accumulator() {
+	public BiConsumer<LinkedHashMap<String, Revision>, Revision> accumulator() {
 		return (accum, rev) -> {
-			String title = rev.getPage();
-			
-			if (accum.containsKey(title)) {
-				accum.remove(title);
-			} else if (rev.isBot()) {
-				return;
-			}
-			
-			accum.put(title, rev.getRevid());
+			accum.put(rev.getPage(), rev);
 		};
 	}
 
 	@Override
-	public BinaryOperator<LinkedHashMap<String, Long>> combiner() {
+	public BinaryOperator<LinkedHashMap<String, Revision>> combiner() {
 		return null;
 	}
 
 	@Override
-	public Function<LinkedHashMap<String, Long>, PageContainer[]> finisher() {
+	public Function<LinkedHashMap<String, Revision>, List<String>> finisher() {
 		return accum -> {
-			Long[] pageids = accum.values().toArray(new Long[accum.size()]);
-			PageContainer[] pages;
-			
-			try {
-				pages = wb.getContentOfRevIds(pageids);
-			} catch (IOException e) {
-				throw new RuntimeException(e);
-			}
-			
-			return accum.keySet().stream()
-				.map(title -> Stream.of(pages).filter(pc -> pc.getTitle().equals(title)).findAny().get())
-				.filter(Objects::nonNull)
-				.toArray(PageContainer[]::new);
+			return accum.values().stream()
+				.filter(rev -> rev.getTimestamp().before(cal))
+				.map(Revision::getPage)
+				.collect(Collectors.toList());
 		};
 	}
 
@@ -166,5 +167,4 @@ class PageContainerCollector implements Collector<Revision, LinkedHashMap<String
 	public Set<java.util.stream.Collector.Characteristics> characteristics() {
 		return Collections.emptySet();
 	}
-	
 }
