cdb098155f3b90efec79f8141cab822b5197fa20
Peter Bowman <bwmn.peter@gmail.com>
2015-08-06 16:15:40 +0200

    introduce EditorBase.addOperation, new class: parsing.Editor

---
+++ wikibot/src/com/github/wikibot/parsing/Editor.java
@@ -0,0 +1,22 @@
+package com.github.wikibot.parsing;
+
+import com.github.wikibot.parsing.eswikt.Page;
+import com.github.wikibot.utils.PageContainer;
+
+public class Editor extends EditorBase {
+
+	public Editor(Page page) {
+		super(page.getTitle(), page.toString());
+	}
+	
+	public Editor(PageContainer pc) {
+		super(pc.getTitle(), pc.getText());
+	}
+
+	@Override
+	public void check() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
