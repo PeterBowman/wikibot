c9834ef113f6291d530b1a5a033128f5c5f1e7ca
Peter Bowman <bwmn.peter@gmail.com>
2015-08-25 00:47:45 +0200

    Review eswikt.ScheduledEditor

--- wikibot/src/com/github/wikibot/scripts/eswikt/ScheduledEditor.java
+++ wikibot/src/com/github/wikibot/scripts/eswikt/ScheduledEditor.java
@@ -1,5 +1,6 @@
 package com.github.wikibot.scripts.eswikt;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,11 +8,13 @@ import java.util.stream.Stream;
 
 import javax.security.auth.login.FailedLoginException;
 
+import org.wikiutils.IOUtils;
 import org.wikiutils.ParseUtils;
 
 import com.github.wikibot.main.ESWikt;
 import com.github.wikibot.parsing.EditorBase;
 import com.github.wikibot.parsing.Page;
+import com.github.wikibot.parsing.ParsingException;
 import com.github.wikibot.parsing.eswikt.Editor;
 import com.github.wikibot.utils.Domains;
 import com.github.wikibot.utils.Login;
@@ -19,6 +22,9 @@ import com.github.wikibot.utils.PageContainer;
 import com.github.wikibot.utils.Users;
 
 public final class ScheduledEditor {
+	private static final String LOCATION = "./data/scripts.eswikt/ScheduledEditor/";
+	private static final String LAST_ENTRY = LOCATION + "last.txt";
+	private static final int BATCH = 500;
 	private static ESWikt wb;
 	private static List<String> errors;
 	
@@ -30,7 +36,7 @@ public final class ScheduledEditor {
 		
 		if (args.length == 0) {
 			final String category = "";
-			processCategory(category);
+			processCategorymembers(category);
 			return;
 		} else {
 			switch (args[0]) {
@@ -39,7 +45,7 @@ public final class ScheduledEditor {
 						return;
 					}
 					
-					processCategory(args[1]);
+					processCategorymembers(args[1]);
 					break;
 				case "-a":
 					processAllpages();
@@ -52,7 +58,7 @@ public final class ScheduledEditor {
 		System.out.printf("%d errors: %s%n", errors.size(), errors);
 	}
 	
-	private static void processCategory(String category) throws IOException {
+	private static void processCategorymembers(String category) throws IOException {
 		PageContainer[] pages = wb.getContentOfCategorymembers(category, ESWikt.MAIN_NAMESPACE);
 		
 		Stream.of(pages)
@@ -61,7 +67,55 @@ public final class ScheduledEditor {
 	}
 	
 	private static void processAllpages() {
+		String lastEntry = getLastSavedEntry();
 		
+		while (true) {
+			PageContainer[] pages;
+			
+			try {
+				pages = wb.listPagesContent(lastEntry, BATCH, ESWikt.MAIN_NAMESPACE);
+			} catch (IOException e) {
+				e.printStackTrace();
+				sleep();
+				continue;
+			} catch (Exception e2) {
+				e2.printStackTrace();
+				return;
+			}
+			
+			if (pages.length < 2) {
+				break;
+			}
+			
+			Stream.of(pages)
+				.limit(pages.length - 1)
+				.filter(ScheduledEditor::filterPages)
+				.forEach(ScheduledEditor::processPage);
+			
+			lastEntry = pages[pages.length - 1].getTitle();
+			saveLastEntry(lastEntry);
+		}
+	}
+	
+	private static String getLastSavedEntry() {
+		String[] lines;
+		
+		try {
+			lines = IOUtils.loadFromFile(LAST_ENTRY, "", "UTF8");
+		} catch (FileNotFoundException e) {
+			return null;
+		}
+		
+		return lines[0];
+	}
+	
+	private static void saveLastEntry(String entry) {
+		try {
+			IOUtils.writeToFile(entry, LAST_ENTRY);
+			System.out.printf("Last entry: %s%n", entry);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	private static boolean filterPages(PageContainer pc) {
@@ -79,22 +133,63 @@ public final class ScheduledEditor {
 			return false;
 		}
 		
-		Page p = Page.wrap(pc);
+		Page p = null;
+		
+		try {
+			p = Page.wrap(pc);
+		} catch (ParsingException e) {
+			onExceptionThrow(pc, e);
+			return false;
+		}
+		
 		return !p.hasSectionWithHeader("^[Ff]orma .*");
 	}
 	
 	private static void processPage(PageContainer pc) {
 		EditorBase editor = new Editor(pc);
-		editor.check();
+		
+		try {
+			editor.check();
+		} catch (ParsingException e) {
+			onExceptionThrow(pc, e);
+			return;
+		}
 		
 		if (editor.isModified()) {
 			try {
-				wb.edit(pc.getTitle(), editor.getPageText(), editor.getSummary(), pc.getTimestamp());
-				System.out.println(editor.getLogs());
+				editEntry(pc, editor);
 			} catch (Exception e) {
-				e.printStackTrace();
+				System.out.printf("ERROR: %s%n", pc.getTitle());
 				errors.add(pc.getTitle());
+				return;
 			}
+			
+			System.out.println(editor.getLogs());
 		}
+	}
+	
+	private static void editEntry(PageContainer pc, EditorBase editor) throws Exception {
+		try {
+			wb.edit(pc.getTitle(), editor.getPageText(), editor.getSummary(), pc.getTimestamp());
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			sleep();
+			editEntry(pc, editor);
+		} catch (Exception e2) {
+			e2.printStackTrace();
+			throw e2;
+		}
+	}
+	
+	private static void sleep() {
+		try {
+			Thread.sleep(1000 * 60 * 5);
+		} catch (InterruptedException e2) {}
+	}
+
+	private static void onExceptionThrow(PageContainer pc, ParsingException e) {
+		System.out.printf("ParsingException in %s%n", pc.getTitle());
+		errors.add(pc.getTitle());
+		e.printStackTrace();
 	}
 }
