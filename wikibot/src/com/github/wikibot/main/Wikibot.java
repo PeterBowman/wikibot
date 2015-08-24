e51eed0cc47e9ce7ed46fa3294631b28ad833a7f
Peter Bowman <bwmn.peter@gmail.com>
2015-08-25 00:46:45 +0200

    Wikibot: add listPagesContent() and getGeneratedContent(String, int)

--- wikibot/src/com/github/wikibot/main/Wikibot.java
+++ wikibot/src/com/github/wikibot/main/Wikibot.java
@@ -185,11 +185,16 @@ public class Wikibot extends WMFWiki {
 			list.addAll(func.apply(line));
 		}
 		
+		log(Level.INFO, "getListedContent", "Successfully retrieved page contents (" + list.size() + " pages)");
 		return list;
 	}
 
 	private PageContainer[] getGeneratedContent(String url) throws IOException {
-		List<PageContainer> list = new ArrayList<PageContainer>(slowmax);
+		return getGeneratedContent(url, -1);
+	}
+	
+	private PageContainer[] getGeneratedContent(String url, int limit) throws IOException {
+		List<PageContainer> list = new ArrayList<PageContainer>(limit);
 		
 		String cont = "continue=";
 		String line;
@@ -198,8 +203,9 @@ public class Wikibot extends WMFWiki {
 	    	line = fetch(url + "&" + cont, "getGeneratedContent");
 	    	cont = parseContinue(line);
 	        list.addAll(parseContentLine(line));
-	    } while (cont != null);
+	    } while (cont != null && (limit < 0 || list.size() < limit));
 		
+		log(Level.INFO, "getGeneratedContent", "Successfully retrieved page contents (" + list.size() + " pages)");
 		return list.toArray(new PageContainer[list.size()]);
 	}
 	
@@ -652,6 +658,27 @@ public class Wikibot extends WMFWiki {
         int size = pages.size();
         log(Level.INFO, "listPages", "Successfully retrieved page list (" + size + " pages)");
         return pages.toArray(new String[size]);
+    }
+    
+    public PageContainer[] listPagesContent(String from, int limit, int... ns) throws IOException
+    {
+        // @revised 0.15 to add short/long pages
+        // No varargs namespace here because MW API only supports one namespace
+        // for this module.
+        StringBuilder url = new StringBuilder(query);
+        url.append("prop=revisions&rvprop=content%7Ctimestamp&generator=allpages");
+        // max and min
+        if (from != null)
+        {
+            url.append("&gapfrom=");
+            url.append(URLEncoder.encode(from, "UTF-8"));
+        }
+        url.append("&gapfilterredir=nonredirects");
+        limit = Math.min(limit, max);
+        url.append("&gaplimit=" + limit);
+
+        constructNamespaceString(url, "gap", ns);		
+        return getGeneratedContent(url.toString(), limit);
     }
     
     public Map<String, List<String[]>> allIwBacklinks() throws IOException {
