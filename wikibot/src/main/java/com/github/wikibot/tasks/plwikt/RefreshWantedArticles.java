64a6ae39ba763b1f0b1d9c2a630135de6600477f
Peter Bowman <bwmn.peter@gmail.com>
2016-06-27 03:24:21 +0200

    Create tasks.plwikt.RefreshWantedArticles

---
+++ wikibot/src/main/java/com/github/wikibot/tasks/plwikt/RefreshWantedArticles.java
@@ -0,0 +1,188 @@
+package com.github.wikibot.tasks.plwikt;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.xml.bind.ValidationException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.nodes.TextNode;
+import org.jsoup.select.Elements;
+
+import com.github.wikibot.main.Wikibot;
+import com.github.wikibot.parsing.AbstractPage;
+import com.github.wikibot.parsing.plwikt.Page;
+import com.github.wikibot.parsing.plwikt.Section;
+import com.github.wikibot.utils.Domains;
+import com.github.wikibot.utils.Login;
+import com.github.wikibot.utils.Misc;
+import com.github.wikibot.utils.PageContainer;
+import com.github.wikibot.utils.Users;
+
+public final class RefreshWantedArticles {
+	private static final String TARGET_PAGE = "Szablon:Potrzebne";
+	private static final int MAX_LENGHT = 100;
+	
+	private static final Pattern P_LINK;
+	private static final Pattern P_OCCURRENCES;
+	
+	private static Wikibot wb;
+	
+	static {
+		P_LINK = Pattern.compile("\\[\\[([^\\]\n]+?)\\]\\]");
+		P_OCCURRENCES = Pattern.compile("((?: *• *)?" + P_LINK.pattern() + ")+");
+	}
+
+	public static void main(String[] args) throws Exception {
+		wb = Login.retrieveSession(Domains.PLWIKT, Users.USER2);
+		
+		String content = wb.getPageText(TARGET_PAGE);
+		Document doc = Jsoup.parseBodyFragment(content);
+		doc.outputSettings().prettyPrint(false);
+		
+		Element targetElement;
+		
+		try {
+			targetElement = selectTargetElement(doc);
+		} catch (ValidationException e) {
+			System.out.println(e.getMessage());
+			return;
+		}
+		
+		List<String> visibleTitles = extractTitles(targetElement.previousSibling());
+		List<String> hiddenTitles = extractTitles(targetElement);
+		
+		if (visibleTitles.isEmpty() || hiddenTitles.isEmpty()) {
+			System.out.println("No titles extracted.");
+			return;
+		}
+		
+		List<String> doneVisible = filterDoneArticles(visibleTitles);
+		
+		if (doneVisible.isEmpty()) {
+			System.out.println("No articles from the visible list have been created yet.");
+			return;
+		}
+		
+		List<String> doneHidden = filterDoneArticles(hiddenTitles);
+		
+		processElement(targetElement, visibleTitles, doneVisible, hiddenTitles, doneHidden);
+		
+		String text = doc.body().html();
+		String counter = Misc.makePluralPL(doneVisible.size(), "utworzone", "utworzonych");
+		String summary = String.format("odświeżenie listy (%s)", counter);
+		
+		wb.setMarkBot(false);
+		wb.edit(TARGET_PAGE, text, summary);
+	}
+	
+	private static Element selectTargetElement(Document doc) throws ValidationException {
+		Elements els = doc.getElementsByTag("noinclude");
+		
+		if (els.isEmpty()) {
+			throw new ValidationException("No <noinclude> tags found.");
+		}
+		
+		Element parent = els.get(0).parent();
+		
+		if (els.stream().anyMatch(el -> el.parent() != parent)) {
+			throw new ValidationException("Multiple <noinclude> tags with no common parent.");
+		}
+		
+		Element targetElement = els.stream()
+			.filter(el -> P_OCCURRENCES.matcher(el.html()).matches())
+			.findFirst()
+			.orElseThrow(() -> new ValidationException("No matching <noinclude> target element found."));
+		
+		if (!targetElement.children().isEmpty()) {
+			throw new ValidationException("Target <noinclude> node has inner elements.");
+		}
+		
+		Node previousSibling = targetElement.previousSibling();
+		
+		if (previousSibling == null) {
+			throw new ValidationException("Target <noinclude> node has no previous sibling.");
+		}
+		
+		if (!(previousSibling instanceof TextNode)) {
+			throw new ValidationException("Target <noinclude> node has no previous TextNode sibling.");
+		}
+		
+		if (!P_OCCURRENCES.matcher(previousSibling.toString()).matches()) {
+			throw new ValidationException("TextNode sibling does not match the expected pattern.");
+		}
+		
+		return targetElement;
+	}
+	
+	private static List<String> extractTitles(Node node) {
+		String text;
+		
+		if (node instanceof Element) {
+			text = ((Element) node).html().trim();
+		} else {
+			text = node.toString().trim();
+		}
+		
+		List<String> list = new ArrayList<>();
+		Matcher m = P_LINK.matcher(text);
+		
+		while (m.find()) {
+			list.add(m.group(1).trim());
+		}
+		
+		return list;
+	}
+	
+	private static List<String> filterDoneArticles(List<String> titles) throws IOException {
+		PageContainer[] pages = wb.getContentOfPages(titles.toArray(new String[titles.size()]));
+		
+		return Stream.of(pages)
+			.map(Page::wrap)
+			.map(Page::getPolishSection)
+			.filter(Optional::isPresent)
+			.map(Optional::get)
+			.map(Section::getContainingPage)
+			.map(Optional::get)
+			.map(AbstractPage::getTitle)
+			.collect(Collectors.toList());
+	}
+	
+	private static void processElement(Element el, List<String> visibleTitles, List<String> doneVisible,
+			List<String> hiddenTitles, List<String> doneHidden) {
+		List<String> visibleList = new ArrayList<>(visibleTitles);
+		visibleList.removeAll(doneVisible);
+		
+		List<String> hiddenList = new ArrayList<>(hiddenTitles);
+		hiddenList.removeAll(doneHidden);
+		
+		while (!hiddenList.isEmpty() && !checkLength(visibleList)) {
+			visibleList.add(hiddenList.remove(0));
+		}
+		
+		String visibleText = buildString(visibleList, "[[%s]]");
+		String hiddenText = " • " + buildString(hiddenList, "[[%s]]");
+		
+		((TextNode) el.previousSibling()).text(visibleText);
+		el.text(hiddenText);
+	}
+	
+	private static boolean checkLength(List<String> list) {
+		return buildString(list, "%s").length() > MAX_LENGHT;
+	}
+	
+	private static String buildString(List<String> list, String fmt) {
+		return list.stream()
+			.map(s -> String.format(fmt, s))
+			.collect(Collectors.joining(" • "));
+	}
+}
