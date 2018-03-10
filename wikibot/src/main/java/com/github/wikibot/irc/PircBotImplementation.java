debc4f882432a9eecc95d96cf001859d1f9f8319
Bartek Łukawski <bwmn.peter@gmail.com>
2018-03-10 17:12:55 +0100

    Implement Telegram message forwarding in IRC bot

--- wikibot/src/main/java/com/github/wikibot/irc/PircBotImplementation.java
+++ wikibot/src/main/java/com/github/wikibot/irc/PircBotImplementation.java
@@ -5,6 +5,11 @@ import java.nio.charset.Charset;
 
 import org.apache.commons.io.FileUtils;
 import org.jibble.pircbot.PircBot;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import com.github.wikibot.telegram.SimpleMessageForwarderBot;
 
 public class PircBotImplementation extends PircBot {
 	private static final String LOCATION = "./data/sessions/";
@@ -12,13 +17,31 @@ public class PircBotImplementation extends PircBot {
 	private static final String SERVER = "irc.freenode.net";
 	private static final String PLWIKT_CHANNEL = "#wiktionary-pl";
 	private static final int RECONNECT_DELAY_MILLIS = 30000;
+	private static final String MAINTAINER_USERNAME = "Peter_Bowman";
+	private static final String TELEGRAM_BOT_USERNAME = "PBtelegram_bot";
 	
-	public PircBotImplementation() {
+	private SimpleMessageForwarderBot telegramBot;
+	
+	private enum Messages {
+		NOTIFY_ON_PING("%s: skontaktuj się ze mną (User:%s) za pośrednictwem bota, wysyłając prywatną wiadomość: /query %s <wiadomość>"),
+		NOTIFY_ON_PM_SUCCESS("wysłano powiadomienie"),
+		NOTIFY_ON_PM_FAILURE("%s: nie udało się wysłać powiadomienia, proszę o kontakt drogą mailową: https://pl.wiktionary.org/wiki/Specjalna:E-mail/%s");
+		
+		public final String str;
+		
+		private Messages(String str) {
+			this.str = str;
+		}
+	}
+	
+	public PircBotImplementation(SimpleMessageForwarderBot telegramBot) {
 		this.setName("PBbot");
+		this.telegramBot = telegramBot;
 	}
 	
 	@Override
-	protected void onKick(String channel, String kickerNick, String kickerLogin, String kickerHostname, String recipientNick, String reason) {
+	protected void onKick(String channel, String kickerNick, String kickerLogin, String kickerHostname,
+			String recipientNick, String reason) {
 		if (recipientNick.equalsIgnoreCase(getNick())) {
 		    joinChannel(channel);
 		}
@@ -44,25 +67,44 @@ public class PircBotImplementation extends PircBot {
 	@Override
 	protected void onMessage(String channel, String sender, String login, String hostname, String message) {
 		if (message.contains("PBbot")) {
-			String reply = String.format("%s: what?", sender);
-			sendMessage(channel, reply);
+			String reply = String.format(Messages.NOTIFY_ON_PING.str, sender, MAINTAINER_USERNAME, getNick());
+			sendNotice(sender, reply);
 		}
 	}
 	
 	@Override
 	protected void onPrivateMessage(String sender, String login, String hostname, String message) {
-		sendMessage(sender, "PM test");
+		try {
+			telegramBot.notifyMaintainer(sender, message);
+			sendNotice(sender, Messages.NOTIFY_ON_PM_SUCCESS.str);
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+			String reply = String.format(Messages.NOTIFY_ON_PM_FAILURE.str, sender, MAINTAINER_USERNAME);
+			sendNotice(sender, reply);
+		}
 	}
 	
 	public static void main(String[] args) throws Exception {
-		PircBotImplementation bot = new PircBotImplementation();
+		File fIrc = new File(LOCATION + "irc.txt");
+		File fTelegramToken = new File(LOCATION + "telegram_token.txt");
+		File fTelegramChatid = new File(LOCATION + "telegram_chatid.txt");
+
+		String password = FileUtils.readFileToString(fIrc, Charset.forName("UTF8"));
+		String telegramToken = FileUtils.readFileToString(fTelegramToken, Charset.forName("UTF8"));
+		String telegramChatid = FileUtils.readFileToString(fTelegramChatid, Charset.forName("UTF8"));
 		
-		File f = new File(LOCATION + "irc.txt");
-		String password = FileUtils.readFileToString(f, Charset.forName("UTF8"));
+		ApiContextInitializer.init();
+		TelegramBotsApi botsApi = new TelegramBotsApi();
+		SimpleMessageForwarderBot telegramBot =
+				new SimpleMessageForwarderBot(TELEGRAM_BOT_USERNAME, telegramToken, Long.parseLong(telegramChatid));
 		
-		bot.setVerbose(true);
-		bot.connect(SERVER, PORT, password);
-		bot.joinChannel(PLWIKT_CHANNEL);
+		botsApi.registerBot(telegramBot);
+		
+		PircBotImplementation ircBot = new PircBotImplementation(telegramBot);
+		
+		ircBot.setVerbose(true);
+		ircBot.connect(SERVER, PORT, password);
+		ircBot.joinChannel(PLWIKT_CHANNEL);
 	}
 
 }
