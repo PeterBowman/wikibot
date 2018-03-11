acf9eb4bab5004e5f38697b4632cec25ad19eb3d
Bartek Łukawski <bwmn.peter@gmail.com>
2018-03-11 14:55:06 +0100

    Write simple main method for debug purposes

--- wikibot/src/main/java/com/github/wikibot/telegram/SimpleMessageForwarderBot.java
+++ wikibot/src/main/java/com/github/wikibot/telegram/SimpleMessageForwarderBot.java
@@ -1,9 +1,14 @@
 package com.github.wikibot.telegram;
 
+import java.io.File;
+import java.nio.charset.Charset;
 import java.util.function.BiConsumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.io.FileUtils;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
@@ -109,6 +114,20 @@ public class SimpleMessageForwarderBot extends TelegramLongPollingBot {
 		SendMessage message = new SendMessage().setChatId(chatId).setText(text).setReplyMarkup(replyKeyboard);
 		lastSender = sender;
 		execute(message);
+	}
+	
+	public static void main(String[] args) throws Exception {
+		final String location = "./data/sessions/";
+		File fTelegramToken = new File(location + "telegram_token.txt");
+		File fTelegramChatid = new File(location + "telegram_chatid.txt");
+
+		String telegramToken = FileUtils.readFileToString(fTelegramToken, Charset.forName("UTF8"));
+		String telegramChatid = FileUtils.readFileToString(fTelegramChatid, Charset.forName("UTF8"));
+		
+		ApiContextInitializer.init();
+		TelegramBotsApi botsApi = new TelegramBotsApi();
+		SimpleMessageForwarderBot bot = new SimpleMessageForwarderBot("PBbot", telegramToken, Long.parseLong(telegramChatid));
+		botsApi.registerBot(bot);
 	}
 	
 }
