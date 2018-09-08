0e404df8fd1bb23e2ee0fa97d71495872ef19242
Bartek Łukawski <bwmn.peter@gmail.com>
2018-09-08 14:03:21 +0200

    Create Gramota online dictionary parser

---
+++ wikibot/src/main/java/com/github/wikibot/utils/Gramota.java
@@ -0,0 +1,59 @@
+package com.github.wikibot.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+
+public class Gramota extends OnlineDict<Gramota> {
+
+	public Gramota(String entry) {
+		super(entry, "http://gramota.ru/slovari/dic/?word=", "windows-1251");
+	}
+
+	@Override
+	public Gramota call() throws Exception {
+		fetchEntry();
+		return this;
+	}
+
+	@Override
+	protected String escape(String text) throws UnsupportedEncodingException {
+		return URLEncoder.encode(text, "UTF-8");
+	}
+
+	@Override
+	protected String stripContent(String text) {
+		return Jsoup.parse(text).getElementsByClass("block-content").html();
+	}
+
+	public boolean exists() {
+		return !content.contains("<h2>Искомое слово отсутствует</h2>") && !content.contains("искомое слово отсутствует");
+	}
+
+	public String withAccent() {
+		for (Element el : Jsoup.parse(content).getElementsByClass("accent")) {
+			Element parent = el.parent();
+			String combined = parent.text().trim();
+
+			if (!combined.equals(entry)) {
+				continue;
+			}
+
+			el.text(el.text() + Character.toString((char)0x0301));
+			return parent.text();
+		}
+
+		return null;
+	}
+
+	public static void main(String[] args) throws Exception {
+		Gramota gramota = new Gramota("милиция");
+		gramota.fetchEntry();
+		System.out.println(gramota.getContent());
+		System.out.println(gramota.exists());
+		System.out.println(gramota.withAccent());
+	}
+
+}
--- wikibot/src/main/java/com/github/wikibot/utils/OnlineDict.java
+++ wikibot/src/main/java/com/github/wikibot/utils/OnlineDict.java
@@ -12,89 +12,92 @@ import java.util.concurrent.Callable;
 public abstract class OnlineDict<T> implements Callable<T> {
 	protected String entry;
 	protected String content;
-	
+
 	protected final String URL;
 	private final String ENCODING;
-	
+
 	public boolean isSerial = false;
 
 	// time to open a connection
-    private final int CONNECTION_CONNECT_TIMEOUT_MSEC = 30000; // 30 seconds
-    // time for the read to take place. (needs to be longer, some connections are slow
-    // and the data volume is large!)
-    private final int CONNECTION_READ_TIMEOUT_MSEC = 180000; // 180 seconds
-    
-    protected abstract String escape(String text) throws UnsupportedEncodingException;
-    protected abstract String stripContent(String text);
-    
-    public OnlineDict(String entry, String url, String encoding) {
-    	this.entry = entry;
-    	this.URL = url;
-    	this.ENCODING = encoding;
-    }
-    
-    public String getEntry() {
-    	return entry;
-    }
-    
-    public void setEntry(String entry) throws IOException {
-    	this.entry = entry;
-    	content = getHTML(entry);
-    }
-    
-    public void fetchEntry() throws IOException {
-    	content = getHTML(entry);
-    }
-    
-    public void fetchEntry(String entry) throws IOException {
-    	this.entry = entry;
-    	content = getHTML(entry);
-    }
-    
-    public String getContent() {
-    	return content;
-    }
-    
-    public String getLink() {
-    	return URL + entry;
-    }
-    
-    protected String fetch(String url) throws IOException
-    {
-        // connect
-        /*URLConnection connection = new URL(url).openConnection();
+	private final int CONNECTION_CONNECT_TIMEOUT_MSEC = 30000; // 30 seconds
+	// time for the read to take place. (needs to be longer, some connections are slow
+	// and the data volume is large!)
+	private final int CONNECTION_READ_TIMEOUT_MSEC = 180000; // 180 seconds
+
+	protected abstract String escape(String text) throws UnsupportedEncodingException;
+	protected abstract String stripContent(String text);
+
+	public OnlineDict(String entry, String url, String encoding) {
+		this.entry = entry;
+		this.URL = url;
+		this.ENCODING = encoding;
+	}
+
+	public String getEntry() {
+		return entry;
+	}
+
+	public void setEntry(String entry) throws IOException {
+		this.entry = entry;
+		content = getHTML(entry);
+	}
+
+	public void fetchEntry() throws IOException {
+		content = getHTML(entry);
+	}
+
+	public void fetchEntry(String entry) throws IOException {
+		this.entry = entry;
+		content = getHTML(entry);
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public String getLink() {
+		return URL + entry;
+	}
+
+	protected String fetch(String url) throws IOException
+	{
+		// connect
+		/*URLConnection connection = new URL(url).openConnection();
         connection.setConnectTimeout(CONNECTION_CONNECT_TIMEOUT_MSEC);
         connection.setReadTimeout(CONNECTION_READ_TIMEOUT_MSEC);
         connection.connect();*/
-    	
-    	HttpURLConnection connection = (HttpURLConnection) (new URL(url).openConnection());
-    	connection.setConnectTimeout(CONNECTION_CONNECT_TIMEOUT_MSEC);
-        connection.setReadTimeout(CONNECTION_READ_TIMEOUT_MSEC);
-        connection.setInstanceFollowRedirects(true);
-        connection.setUseCaches(false);
-        connection.connect();
-               
-        // get the text
-        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName(ENCODING)));
-        
-        String line;
-        StringBuilder text = new StringBuilder(50000);
-        
-        while ((line = in.readLine()) != null)
-            text.append(line).append("\n");
-        
-        in.close();
-        String temp = text.toString();
-                
-        return temp;
-    }
-	
+
+		HttpURLConnection connection = (HttpURLConnection) (new URL(url).openConnection());
+		connection.setConnectTimeout(CONNECTION_CONNECT_TIMEOUT_MSEC);
+		connection.setReadTimeout(CONNECTION_READ_TIMEOUT_MSEC);
+		connection.setInstanceFollowRedirects(true);
+		connection.setUseCaches(false);
+		connection.connect();
+
+		// get the text
+		BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream(), Charset.forName(ENCODING)));
+
+		String line;
+		StringBuilder text = new StringBuilder(50000);
+
+		while ((line = in.readLine()) != null)
+			text.append(line).append("\n");
+
+		in.close();
+		String temp = text.toString();
+
+		return temp;
+	}
+
 	protected String getHTML(String page) throws IOException {
-		String text = fetch(URL + escape(page));
+		String target = URL + escape(page);
+		System.out.println(target);
+
+		String text = fetch(target);
 		text = text.replace("\n", "");
 		int a = text.indexOf("<body");
 		int b = text.indexOf("</body>", a) + 7;
-		
+
 		return stripContent(text.substring(a, b));
 	}
 }
