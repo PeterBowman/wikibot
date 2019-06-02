fdc909acc722078f42b5da423e7fe1486f2b11be
Bartek Łukawski <bwmn.peter@gmail.com>
2019-06-02 02:52:50 +0200

    Embrace JDK 9+ modules, solve XML module hell, delete HTMLUnit
    Also upgrade maven compiler plugin to illegal operation warnings:
    https://github.com/x-stream/xstream/issues/101#issuecomment-406808501

---
+++ wikibot/src/main/java/module-info.java
@@ -0,0 +1,36 @@
+module org.github.wikibot {
+	exports com.github.wikibot.irc;
+	exports com.github.wikibot.parsing.eswikt;
+	exports com.github.wikibot.dumps;
+	exports com.github.wikibot.utils;
+	exports com.github.wikibot.parsing.plwikt;
+	exports com.github.wikibot.parsing;
+	exports com.github.wikibot.telegram;
+	exports com.github.wikibot.main;
+
+	requires com.google.common;
+	requires com.ibm.icu;
+	requires commons.cli;
+	requires java.logging;
+	requires java.sql;
+	requires java.xml;
+	requires org.apache.commons.collections4;
+	requires org.apache.commons.compress;
+	requires org.apache.commons.io;
+	requires org.apache.commons.lang3;
+	requires org.apache.commons.text;
+	requires org.json;
+	requires org.jsoup;
+	requires pircbot;
+	requires plural4j;
+	requires telegrambots;
+	requires telegrambots.meta;
+	requires univocity.parsers;
+	requires org.wikipedia;
+	requires xstream;
+	
+	// https://stackoverflow.com/a/41265267
+	opens com.github.wikibot.tasks.eswikt;
+	opens com.github.wikibot.tasks.plwiki;
+	opens com.github.wikibot.tasks.plwikt;
+}
