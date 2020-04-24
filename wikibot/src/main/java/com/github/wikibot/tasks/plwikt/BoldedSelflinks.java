3e7756c23a86c5eac32d89b4a0173c78aeba1e34
Bartek Łukawski <bwmn.peter@gmail.com>
2020-04-24 16:36:52 +0200

    Create tasks.plwikt.BoldedSelflinks

---
+++ wikibot/src/main/java/com/github/wikibot/tasks/plwikt/BoldedSelflinks.java
@@ -0,0 +1,275 @@
+package com.github.wikibot.tasks.plwikt;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.OpenOption;
+import java.nio.file.StandardOpenOption;
+import java.text.Collator;
+import java.text.SimpleDateFormat;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+import com.github.wikibot.dumps.XMLDumpReader;
+import com.github.wikibot.dumps.XMLRevision;
+import com.github.wikibot.main.Wikibot;
+import com.github.wikibot.parsing.plwikt.FieldTypes;
+import com.github.wikibot.parsing.plwikt.Page;
+import com.github.wikibot.utils.Login;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+
+public final class BoldedSelflinks {
+	private static final String LOCATION = "./data/tasks.plwikt/BoldedSelflinks/";
+	private static final String TARGET_PAGE = "Wikipedysta:PBbot/pogrubione selflinki";
+	
+	// https://pl.wiktionary.org/wiki/MediaWiki:Gadget-section-links.js
+	private static final List<String> IGNORED_SELFLINKS = List.of(
+		"znak chiński", "chiński standardowy", "minnan", "japoński", "gan", "ajnoski", "kantoński"
+	);
+	
+	private static final List<String> IGNORED_LANGS = List.of("jidysz", "niemiecki");
+	
+	private static final Map<FieldTypes, List<String>> IGNORED_FIELDS = Map.of(
+		FieldTypes.EXAMPLES, Collections.emptyList(),
+		FieldTypes.TRANSLATIONS, Collections.emptyList(),
+		FieldTypes.ETYMOLOGY, Collections.emptyList(),
+		FieldTypes.DERIVED_TERMS, Collections.emptyList(),
+		FieldTypes.NOTES, Collections.emptyList(),
+		FieldTypes.DEFINITIONS, List.of("polski")
+	);
+	
+	// from Linker::formatLinksInComment in Linker.php
+	private static final Pattern P_LINK = Pattern.compile("\\[\\[:?([^\\]|]+)(?:\\|((?:]?[^\\]|])*+))*\\]\\]([^\\[]*)");
+	private static final Pattern P_TRANSL = Pattern.compile("→[^•;]+");
+	
+	private static final Collator COLL_PL = Collator.getInstance(new Locale("pl"));
+	
+	private static final String PAGE_INTRO;
+	
+	private static Wikibot wb;
+	
+	static {
+		String excludedSelflinks = IGNORED_SELFLINKS.stream().collect(Collectors.joining(", "));
+		String excludedLangs = IGNORED_LANGS.stream().collect(Collectors.joining(", "));
+		
+		String excludedFields = IGNORED_FIELDS.entrySet().stream()
+			.map(e -> !e.getValue().isEmpty()
+				? String.format(
+					"%s (wyjątki: %s)",
+					e.getKey().localised(),
+					e.getValue().stream().collect(Collectors.joining(", "))
+				)
+				: e.getKey().localised())
+			.collect(Collectors.joining(", "));
+		
+		PAGE_INTRO =
+			"Zestawienie wystąpień pogrubionych selflinków, czyli linków prowadzących do strony, w której " +
+			"się znajdują. Pogrubienie jest zazwyczaj wyłączone za sprawą [[MediaWiki:Gadget-section-links.js|skryptu JS]], " +
+			"o ile link prowadzi do innej sekcji językowej. Wykluczenia:\n" +
+			"* języki z wyłączoną obsługą selflinków przez JS – " + excludedSelflinks + ";\n" +
+			"* języki źle współpracujące z mechanizmem selflinków – " + excludedLangs + ";\n" +
+			"* pola – " + excludedFields + ".\n" +
+			"\n" +
+			"Dane na podstawie zrzutu z bazy danych z dnia $1. Aktualizacja: ~~~~~.";;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		wb = Login.createSession("pl.wiktionary.org");
+		
+		XMLDumpReader reader = getXMLReader(args);
+		List<Item> list = analyzeDump(reader);
+		
+		if (!checkAndUpdateStoredData(list)) {
+			System.out.println("No changes detected, aborting.");
+			return;
+		}
+		
+		String timestamp = extractTimestamp(reader.getFile());
+		String pageText = makePageText(list, timestamp);
+		
+		wb.setMarkBot(false);
+		wb.edit(TARGET_PAGE, pageText, "aktualizacja");
+	}
+	
+	private static XMLDumpReader getXMLReader(String[] args) throws ParseException, FileNotFoundException {
+		if (args.length != 0) {
+			Options options = new Options();
+			options.addOption("d", "dump", true, "read from dump file");
+			
+			CommandLineParser parser = new DefaultParser();
+			CommandLine line = parser.parse(options, args);
+			
+			if (line.hasOption("dump")) {
+				String pathToFile = line.getOptionValue("dump");
+				return new XMLDumpReader(new File(pathToFile));
+			} else {
+				new HelpFormatter().printHelp(BoldedSelflinks.class.getName(), options);
+				throw new IllegalArgumentException();
+			}
+		} else {
+			return new XMLDumpReader("pl.wiktionary.org");
+		}
+	}
+	
+	private static List<Item> analyzeDump(XMLDumpReader reader) throws IOException {
+		int size = wb.getSiteStatistics().get("pages");
+		final Pattern pNewline = Pattern.compile("\n");
+		
+		try (Stream<XMLRevision> stream = reader.getStAXReader(size).stream()) {
+			return stream.parallel()
+				.filter(XMLRevision::nonRedirect)
+				.filter(XMLRevision::isMainNamespace)
+				.map(rev -> Page.store(rev.getTitle(), rev.getText()))
+				.flatMap(p -> p.getAllSections().stream()
+					.filter(s -> !IGNORED_SELFLINKS.contains(s.getLangShort()))
+					.filter(s -> !IGNORED_LANGS.contains(s.getLangShort()))
+					.flatMap(s -> s.getAllFields().stream()
+						.filter(f -> !f.isEmpty())
+						.filter(f -> IGNORED_FIELDS.entrySet().stream()
+							.allMatch(e -> f.getFieldType() != e.getKey() || e.getValue().contains(s.getLangShort()))
+						)
+						.flatMap(f -> pNewline.splitAsStream(f.getContent())
+							.filter(line -> filterLines(line, p.getTitle()))
+							.map(line ->
+								new Item(p.getTitle(), s.getLangShort(), f.getFieldType().localised(), line)
+							)
+						)
+					)
+				)
+				.sorted((i1, i2) -> COLL_PL.compare(i1.title, i2.title))
+				.collect(Collectors.toList());
+		}
+	}
+	
+	private static boolean filterLines(String line, String title) {
+		if (line.contains("→")) {
+			line = P_TRANSL.matcher(line).replaceAll("");
+		}
+		
+		Matcher m = P_LINK.matcher(line);
+		
+		while (m.find()) {
+			String target = m.group(1).trim().replaceFirst("#.*", ""); // ignore URL fragments
+			
+			if (target.equals(title)) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	private static boolean checkAndUpdateStoredData(List<Item> list) throws FileNotFoundException, IOException {
+		int newHashCode = list.hashCode();
+		int storedHashCode;
+		
+		File fHash = new File(LOCATION + "hash.txt");
+		File fList = new File(LOCATION + "list.xml");
+		
+		try {
+			storedHashCode = Integer.parseInt(Files.readString(fHash.toPath()));
+		} catch (IOException | NumberFormatException e) {
+			e.printStackTrace();
+			storedHashCode = 0;
+		}
+		
+		XStream xstream = new XStream();
+		xstream.processAnnotations(Item.class);
+		
+		if (storedHashCode != newHashCode) {
+			OpenOption[] options = new OpenOption[] {StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING};
+			Files.writeString(fHash.toPath(), Integer.toString(newHashCode), options);
+			Files.writeString(fList.toPath(), xstream.toXML(list), options);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	private static String extractTimestamp(File f) {
+		String fileName = f.getName();
+		Pattern patt = Pattern.compile("^[a-z]+-(\\d+)-.+");
+		String errorString = String.format("(błąd odczytu sygnatury czasowej, plik ''%s'')", fileName);
+		
+		Matcher m = patt.matcher(fileName);
+		
+		if (!m.matches()) {
+			return errorString;
+		}
+		
+		String canonicalTimestamp = m.group(1);
+		
+		try {
+			SimpleDateFormat originalDateFormat = new SimpleDateFormat("yyyyMMdd");
+			Date date = originalDateFormat.parse(canonicalTimestamp);
+			SimpleDateFormat desiredDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+			return desiredDateFormat.format(date);
+		} catch (java.text.ParseException e) {
+			return errorString;
+		}
+	}
+	
+	private static String makePageText(List<Item> list, String timestamp) {
+		String output = list.stream()
+			.collect(Collectors.groupingBy(
+				item -> item.title,
+				() -> new TreeMap<>(COLL_PL),
+				Collectors.mapping(
+					item -> String.format(
+						"#:(%s, %s) <nowiki>%s</nowiki>",
+						item.language, item.fieldType, item.line
+					),
+					Collectors.toList()
+				)
+			))
+			.entrySet().stream()
+			.map(entry -> String.format("#[[%s]]%n%s", entry.getKey(), String.join("\n", entry.getValue())))
+			.collect(Collectors.joining("\n"));
+		
+		return PAGE_INTRO.replace("$1", timestamp) + "\n\n" + output;
+	}
+	
+	@XStreamAlias("item")
+	private static class Item {
+		String title;
+		String language;
+		String fieldType;
+		String line;
+		
+		Item(String title, String language, String fieldType, String line) {
+			this.title = title;
+			this.language = language;
+			this.fieldType = fieldType;
+			this.line = line;
+		}
+		
+		@Override
+		public String toString() {
+			return String.format("[%s,%s,%s,%s]", title, language, fieldType, line);
+		}
+		
+		@Override
+		public int hashCode() {
+			return
+				title.hashCode() + language.hashCode() +
+				fieldType.hashCode() + line.hashCode();
+		}
+	}
+}
