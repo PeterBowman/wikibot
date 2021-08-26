7375366e4fe2da167b20bceb9ebd617e0768fe40
Bartek Łukawski <bwmn.peter@gmail.com>
2021-08-26 13:59:15 +0200

    Implement language filter

--- wikibot/src/main/java/com/github/wikibot/scripts/AssistedEdit.java
+++ wikibot/src/main/java/com/github/wikibot/scripts/AssistedEdit.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -23,6 +24,7 @@ import com.github.wikibot.main.Wikibot;
 import com.github.wikibot.parsing.plwikt.Field;
 import com.github.wikibot.parsing.plwikt.FieldTypes;
 import com.github.wikibot.parsing.plwikt.Page;
+import com.github.wikibot.parsing.plwikt.Section;
 import com.github.wikibot.utils.Login;
 import com.github.wikibot.utils.Misc;
 import com.github.wikibot.utils.PageContainer;
@@ -34,7 +36,7 @@ public final class AssistedEdit {
 	private static final Path WORKLIST = LOCATION.resolve("worklist.txt");
 	private static final Path TIMESTAMPS = LOCATION.resolve("timestamps.xml");
 	private static final Path HASHCODES = LOCATION.resolve("hash.xml");
-	private static final String WORKLIST_FIELD_FORMAT = "worklist-%s.txt";
+	private static final String WORKLIST_FILTERED_FORMAT = "worklist-%s-%s.txt";
 	
 	private static Wikibot wb;
 	private static XStream xstream;
@@ -46,6 +48,7 @@ public final class AssistedEdit {
 		options.addOption("s", "summary", true, "edit summary");
 		options.addOption("m", "minor", false, "mark edits as minor");
 		options.addOption("t", "throttle", true, "set edit throttle [ms]");
+		options.addOption("l", "language", true, "short language name (only pl.wiktionary.org)");
 		options.addOption("f", "field", true, "field type (only pl.wiktionary.org)");
 		
 		if (args.length == 0) {
@@ -58,9 +61,15 @@ public final class AssistedEdit {
 		
 		String domain = line.getOptionValue("domain");
 		
+		if (line.hasOption("language") && !domain.equals("pl.wiktionary.org")) {
+			throw new IllegalArgumentException("Section parser was requested, but project is not pl.wiktionary.org.");
+		}
+		
 		if (line.hasOption("field") && !domain.equals("pl.wiktionary.org")) {
 			throw new IllegalArgumentException("Field parser was requested, but project is not pl.wiktionary.org.");
 		}
+		
+		String sectionName = line.getOptionValue("language");
 		
 		String fieldName = line.getOptionValue("field");
 		FieldTypes fieldType = null;
@@ -80,10 +89,10 @@ public final class AssistedEdit {
 		
 		switch (line.getOptionValue("op")) {
 			case "query":
-				getContents(fieldType);
+				getContents(sectionName, fieldType);
 				break;
 			case "apply":
-				applyChanges(fieldType, line.getOptionValue("summary"), line.hasOption("minor"));
+				applyChanges(sectionName, fieldType, line.getOptionValue("summary"), line.hasOption("minor"));
 				break;
 			default:
 				new HelpFormatter().printHelp(AssistedEdit.class.getName(), options);
@@ -91,7 +100,7 @@ public final class AssistedEdit {
 		}
 	}
 	
-	public static void getContents(FieldTypes fieldType) throws IOException {
+	public static void getContents(String sectionName, FieldTypes fieldType) throws IOException {
 		List<String> titles = Files.readAllLines(TITLES).stream()
 			.map(String::trim)
 			.filter(line -> !line.isEmpty())
@@ -132,43 +141,71 @@ public final class AssistedEdit {
 		
 		Files.writeString(HASHCODES, xstream.toXML(hashcodes));
 		
-		if (fieldType != null) {
-			Map<String, String> mapFields = pages.stream()
+		if (sectionName != null || fieldType != null) {
+			Map<String, String> filteredMap;
+			
+			var stream = pages.stream()
 				.map(Page::wrap)
-				.flatMap(p -> p.getAllSections().stream())
-				.flatMap(s -> s.getField(fieldType).stream())
-				.filter(f -> !f.isEmpty())
-				.collect(Collectors.toMap(
-					f -> String.format("%s # %s",
-						f.getContainingSection().get().getContainingPage().get().getTitle(),
-						f.getContainingSection().get().getLangShort()),
-					Field::getContent,
+				.flatMap(p -> p.getAllSections().stream());
+			
+			if (sectionName != null) {
+				stream = stream.filter(s -> s.getLangShort().equals(sectionName));
+			}
+			
+			if (fieldType != null) {
+				filteredMap = stream
+					.flatMap(s -> s.getField(fieldType).stream())
+					.filter(f -> !f.isEmpty())
+					.collect(Collectors.toMap(
+						f -> String.format("%s # %s",
+							f.getContainingSection().get().getContainingPage().get().getTitle(),
+							f.getContainingSection().get().getLangShort()),
+						Field::getContent,
+						(a, b) -> a,
+						LinkedHashMap::new
+					));
+			} else {
+				filteredMap = stream.collect(Collectors.toMap(
+					s -> String.format("%s # %s",
+						s.getContainingPage().get().getTitle(),
+						s.getLangShort()),
+					Section::toString,
 					(a, b) -> a,
 					LinkedHashMap::new
 				));
+			}
 			
-			String filename = String.format(WORKLIST_FIELD_FORMAT, fieldType.localised());
-			Files.writeString(LOCATION.resolve(filename), Misc.makeList(mapFields));
+			String fieldName = Optional.ofNullable(fieldType).map(FieldTypes::localised).orElse(null);
+			String filename = String.format(WORKLIST_FILTERED_FORMAT, sectionName, fieldName);
+			Files.writeString(LOCATION.resolve(filename), Misc.makeList(filteredMap));
 		}
 	}
 	
-	public static void applyChanges(FieldTypes fieldType, String summary, boolean minor) throws IOException {
+	public static void applyChanges(String sectionName, FieldTypes fieldType, String summary, boolean minor) throws IOException {
 		Map<String, String> map = Misc.readList(Files.readAllLines(WORKLIST).toArray(String[]::new));
 		final var initialSize = map.size();
 		System.out.printf("Size: %d%n", initialSize);
 		
-		if (fieldType != null) {
-			String filename = String.format(WORKLIST_FIELD_FORMAT, fieldType.localised());
-			Map<String, String> mapFields = Misc.readList(Files.readAllLines(LOCATION.resolve(filename)).toArray(String[]::new));
+		if (sectionName != null || fieldType != null) {
+			String fieldName = Optional.ofNullable(fieldType).map(FieldTypes::localised).orElse(null);
+			String filename = String.format(WORKLIST_FILTERED_FORMAT, sectionName, fieldName);
+			Map<String, String> filteredMap = Misc.readList(Files.readAllLines(LOCATION.resolve(filename)).toArray(String[]::new));
 			
-			mapFields.entrySet().forEach(e -> map.computeIfPresent(
+			filteredMap.entrySet().forEach(e -> map.computeIfPresent(
 				e.getKey().substring(0,  e.getKey().indexOf('#') - 1),
 				(title, oldText) -> {
 					var page = Page.store(title, oldText);
-					var sectionName = e.getKey().substring(e.getKey().indexOf('#') + 2);
-					var field = page.getSection(sectionName, true).get().getField(fieldType).get();
+					var langShort = e.getKey().substring(e.getKey().indexOf('#') + 2);
+					var section = page.getSection(langShort, true).get();
 					
-					field.editContent(e.getValue());
+					if (fieldType != null) {
+						var field = section.getField(fieldType).get();
+						field.editContent(e.getValue());
+					} else {
+						var newSection = Section.parse(e.getValue());
+						section.replaceWith(newSection);
+					}
+					
 					return page.toString();
 				}
 			));
@@ -212,7 +249,7 @@ public final class AssistedEdit {
 		Files.move(WORKLIST, WORKLIST.resolveSibling("done.txt"), StandardCopyOption.REPLACE_EXISTING);
 		
 		if (fieldType != null) {
-			String oldFilename = String.format(WORKLIST_FIELD_FORMAT, fieldType.localised());
+			String oldFilename = String.format(WORKLIST_FILTERED_FORMAT, fieldType.localised());
 			String newFilename = String.format("done-%s.txt", fieldType.localised());
 			Files.move(WORKLIST.resolveSibling(oldFilename), WORKLIST.resolveSibling(newFilename), StandardCopyOption.REPLACE_EXISTING);
 		}
