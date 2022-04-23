68ad72520931c39eb072a59e9ffff370f4a7dc3c
Bartek Łukawski <bwmn.peter@gmail.com>
2022-04-23 14:04:28 +0200

    Conform to community feedback

--- wikibot/src/main/java/com/github/wikibot/tasks/plwikt/ArchiveThread.java
+++ wikibot/src/main/java/com/github/wikibot/tasks/plwikt/ArchiveThread.java
@@ -1,5 +1,6 @@
 package com.github.wikibot.tasks.plwikt;
 
+import java.io.IOException;
 import java.time.OffsetDateTime;
 import java.time.Period;
 import java.time.ZonedDateTime;
@@ -10,15 +11,23 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 
+import javax.security.auth.login.LoginException;
+
+import org.jsoup.Jsoup;
+import org.wikipedia.Wiki;
 import org.wikiutils.ParseUtils;
 
+import com.github.plural4j.Plural;
+import com.github.plural4j.Plural.WordForms;
 import com.github.wikibot.main.Wikibot;
 import com.github.wikibot.parsing.Page;
 import com.github.wikibot.parsing.Section;
 import com.github.wikibot.utils.Login;
+import com.github.wikibot.utils.PluralRules;
 
 public final class ArchiveThread {
     private static final List<String> TARGET_PAGES = List.of("Wikisłownik:Zgłoś błąd w haśle");
@@ -26,13 +35,25 @@ public final class ArchiveThread {
 
     private static final Period MIN_PERIOD = Period.ofDays(30);
 
-    private static final Pattern P_HEADER = Pattern.compile("^\\[{2}:?([^]]+?)(?:#[^]]+?)?\\]{2}$");
+    // from CommentParser::doWikiLinks()
+    private static final Pattern P_HEADER_LINK = Pattern.compile("\\[{2}\\s*+:?([^\\[\\]\\|]+)(?:\\|((?:]?[^\\]])*+))?\\]{2}");
     private static final Pattern P_TIMESTAMP = Pattern.compile("\\d{2}:\\d{2}, \\d{1,2} [a-ząćęłńóśźż]{3} \\d{4} \\(CES?T\\)");
 
     private static final DateTimeFormatter DT_FORMATTER = DateTimeFormatter.ofPattern("HH:mm, d LLL yyyy (z)").withLocale(new Locale("pl"));
 
+    private static final String TALK_HEADER_FORMAT = "Przeniesione z [[Specjalna:Niezmienny link/%d#%s|%s]]";
+
     private static final Wikibot wb = Wikibot.newSession("pl.wiktionary.org");
     private static final OffsetDateTime refDateTime = OffsetDateTime.now().minus(MIN_PERIOD);
+    private static final Plural SUMMARY_FORMATTER;
+
+    static {
+        var threads = new WordForms[] {
+            new WordForms(new String[] {"wątek", "wątki", "wątków"})
+        };
+
+        SUMMARY_FORMATTER = new Plural(PluralRules.POLISH, threads);
+    }
 
     public static void main(String[] args) throws Exception {
         Login.login(wb);
@@ -40,47 +61,80 @@ public final class ArchiveThread {
 
         for (var title : TARGET_PAGES) {
             var rev = wb.getTopRevision(title);
-            var text = rev.getText();
-            var page = Page.store(title, text);
+            var page = Page.store(title, rev.getText());
+            var numThreads = processPage(page, rev.getID());
 
-            if (processPage(page, rev.getID())) {
-                wb.edit(title, page.toString(), "archiwizacja", rev.getTimestamp());
+            if (numThreads > 0) {
+                var summary = String.format("zarchiwizowano %d %s", numThreads, SUMMARY_FORMATTER.pl(numThreads, "wątek"));
+                wb.edit(title, page.toString(), summary, rev.getTimestamp());
             }
         }
     }
 
-    private static boolean processPage(Page page, long revid) {
+    private static int processPage(Page page, long revid) throws LoginException, IOException {
         var detachedSections = new ArrayList<Section>();
+        var archivedSections = new ArrayList<Section>();
 
-        for (var section : page.getAllSections()) {
-            var header = section.getHeader();
+        for (var section : page.getAllSections().stream().filter(s -> s.getLevel() == 2).toList()) {
             var text = ParseUtils.removeCommentsAndNoWikiText(section.toString());
-            var m = P_HEADER.matcher(header);
+            var header = section.getHeader();
 
-            if (m.matches() && TARGET_TEMPLATES.stream().anyMatch(template -> !ParseUtils.getTemplates(template, text).isEmpty())) {
-                var title = m.toMatchResult().group(1).trim();
-                var timestamps = retrieveTimestamps(text);
+            var targets = P_HEADER_LINK.matcher(header).results()
+                .map(m -> m.group(1).trim())
+                .filter(target -> wb.namespace(target) == Wiki.MAIN_NAMESPACE)
+                .distinct()
+                .toList();
 
-                if (!timestamps.isEmpty() && timestamps.get(0).isBefore(refDateTime)) {
-                    try {
-                        if ((Boolean) wb.getPageInfo(List.of(title)).get(0).get("exists")) {
-                            var talkPage = wb.getTalkPage(title);
-                            var summary = String.format("Przeniesione z [[Specjalna:Niezmienny link/%d#%s|%s]]",
-                                    revid, title, page.getTitle());
+            var timestamps = retrieveTimestamps(text);
 
-                            wb.newSection(talkPage, summary, section.getFlattenedContent(), false, true);
-                            detachedSections.add(section);
-                        }
-                    } catch (Throwable t) {
-                        t.printStackTrace();
-                        continue;
+            if (hasDoneTemplate(text) && !timestamps.isEmpty() && timestamps.get(0).isBefore(refDateTime)) {
+                var mustArchive = true;
+
+                for (var info : wb.getPageInfo(targets)) {
+                    if ((Boolean)info.get("exists")) {
+                        var pageName = (String)info.get("pagename");
+                        var talkPageName = wb.getTalkPage(pageName);
+                        var summary = String.format(TALK_HEADER_FORMAT, revid, pageName, page.getTitle());
+
+                        wb.newSection(talkPageName, summary, section.getFlattenedContent(), false, true);
+                        mustArchive = false;
                     }
                 }
+
+                if (mustArchive) {
+                    archivedSections.add(section);
+                }
+
+                detachedSections.add(section);
             }
         }
 
+        if (!archivedSections.isEmpty()) {
+            var title = page.getTitle() + "/archiwum";
+            var text = Optional.ofNullable(wb.getPageText(List.of(title)).get(0)).orElse("");
+            var archive = Page.store(title, text);
+
+            archive.appendSections(archivedSections);
+
+            var numThreads = archivedSections.size();
+            var summary = String.format("przeniesiono %d %s do archiwum", numThreads, SUMMARY_FORMATTER.pl(numThreads, "wątek"));
+
+            wb.edit(title, archive.toString(), summary);
+        }
+
         detachedSections.forEach(Section::detach);
-        return !detachedSections.isEmpty();
+        return detachedSections.size();
+    }
+
+    private static boolean hasDoneTemplate(String text) {
+        var doc = Jsoup.parseBodyFragment(text);
+        doc.getElementsByTag("nowiki").remove(); // already removed along with comments, but why not
+        doc.getElementsByTag("s").remove();
+        doc.getElementsByTag("pre").remove();
+        doc.getElementsByTag("syntaxhighlight").remove();
+        doc.getElementsByTag("source").remove();
+
+        return TARGET_TEMPLATES.stream().anyMatch(template -> !ParseUtils.getTemplates(template, doc.text()).isEmpty());
     }
 
     private static List<OffsetDateTime> retrieveTimestamps(String text) {
