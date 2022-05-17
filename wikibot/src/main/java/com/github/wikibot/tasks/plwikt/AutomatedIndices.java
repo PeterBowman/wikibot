fb021ca5e4a119553b6d97eec617c02d434b4004
Bartek Łukawski <bwmn.peter@gmail.com>
2022-05-17 16:20:44 +0200

    Don't ignore category-less entries, clean stuff

--- wikibot/src/main/java/com/github/wikibot/tasks/plwikt/AutomatedIndices.java
+++ wikibot/src/main/java/com/github/wikibot/tasks/plwikt/AutomatedIndices.java
@@ -14,14 +14,10 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.TreeMap;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
+import java.util.function.UnaryOperator;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
-import org.apache.commons.lang3.StringUtils;
-import org.wikiutils.ParseUtils;
 
 import com.github.wikibot.dumps.XMLDumpReader;
 import com.github.wikibot.dumps.XMLRevision;
@@ -36,6 +32,9 @@ import com.ibm.icu.text.Collator;
 import com.ibm.icu.text.Transliterator;
 import com.ibm.icu.util.ULocale;
 import com.thoughtworks.xstream.XStream;
+
+import org.apache.commons.lang3.StringUtils;
+import org.wikiutils.ParseUtils;
 
 public final class AutomatedIndices {
     private static final Path LOCATION = Paths.get("./data/tasks.plwikt/AutomatedIndices/");
@@ -52,17 +51,17 @@ public final class AutomatedIndices {
 
     private static final Map<String, String> languageToIcuCode = Map.of("łaciński", "la", "nowogrecki", "el");
 
-    private static List<String> errors = new ArrayList<>();
+    private static final List<String> errors = new ArrayList<>();
 
     private static final Wikibot wb = Wikibot.newSession("pl.wiktionary.org");
 
     public static void main(String[] args) throws Exception {
         Login.login(wb);
 
-        String pageText = wb.getPageText(List.of(WORKLIST)).get(0);
-        List<String> templates = ParseUtils.getTemplatesIgnoreCase(TEMPLATE, pageText);
+        var pageText = wb.getPageText(List.of(WORKLIST)).get(0);
+        var templates = ParseUtils.getTemplatesIgnoreCase(TEMPLATE, pageText);
 
-        Set<Entry> entries = templates.stream()
+        var entries = templates.stream()
             .map(Entry::parseTemplate)
             .filter(Objects::nonNull)
             .collect(Collectors.toCollection(HashSet::new));
@@ -75,28 +74,28 @@ public final class AutomatedIndices {
 
         validateEntries(entries);
 
-        Map<String, List<Entry>> langToEntries = entries.stream()
+        var langToEntries = entries.stream()
             .flatMap(entry -> entry.languageTemplates.stream())
             .distinct()
             .collect(Collectors.toMap(
-                lang -> lang,
-                lang -> entries.stream().filter(e -> e.languageTemplates.contains(lang)).toList()
+                UnaryOperator.identity(),
+                lang -> entries.stream().filter(e -> e.languageTemplates().contains(lang)).toList()
             ));
 
-        Map<String, ULocale> langToLocale = langToEntries.keySet().stream()
+        var langToLocale = langToEntries.keySet().stream()
             .map(AutomatedIndices::stripLanguagePrefix)
             .collect(Collectors.toMap(
-                lang -> lang,
+                UnaryOperator.identity(),
                 AutomatedIndices::getLocale
             ));
 
         System.out.println(langToLocale);
 
-        XMLDumpReader reader = getDumpReader(args);
-        ConcurrentMap<String, List<String>> indexToTitles = new ConcurrentHashMap<>();
-        ConcurrentMap<String, String> indexToLang = new ConcurrentHashMap<>();
+        var reader = getDumpReader(args);
+        var indexToTitles = new HashMap<String, List<String>>();
+        var indexToLang = new HashMap<String, String>();
 
-        try (Stream<XMLRevision> stream = reader.getStAXReaderStream()) {
+        try (var stream = reader.getStAXReaderStream()) {
             stream
                 .filter(XMLRevision::isMainNamespace)
                 .filter(XMLRevision::nonRedirect)
@@ -108,32 +107,28 @@ public final class AutomatedIndices {
                 .forEach(f -> processDefinitionsField(f, langToEntries, indexToTitles, indexToLang));
         }
 
-        indexToTitles.entrySet().parallelStream()
+        indexToTitles.entrySet().stream()
             .forEach(e -> Collections.sort(
                 e.getValue(),
                 Collator.getInstance(langToLocale.get(indexToLang.get(e.getKey())))
             ));
 
-        Path hash = LOCATION.resolve("hash.xml");
+        var hash = LOCATION.resolve("hash.xml");
 
         @SuppressWarnings("unchecked")
         var indexToHash = Files.exists(hash) ? (Map<String, Integer>) new XStream().fromXML(hash.toFile()) : new HashMap<String, Integer>();
 
-        final String summary = String.format("aktualizacja na podstawie zrzutu z bazy danych: %s", reader.getPathToDump().getFileName());
+        final var summary = String.format("aktualizacja na podstawie zrzutu z bazy danych: %s", reader.getPathToDump().getFileName());
 
-        wb.setMarkBot(true);
-        wb.setMarkMinor(false);
-        wb.setThrottle(1000);
-
-        for (Map.Entry<String, List<String>> e : indexToTitles.entrySet()) {
-            String index = e.getKey();
-            List<String> titles = e.getValue();
-            int newHash = titles.hashCode();
+        for (var e : indexToTitles.entrySet()) {
+            var index = e.getKey();
+            var titles = e.getValue();
+            var newHash = titles.hashCode();
 
             if (!indexToHash.containsKey(index) || indexToHash.get(index) != newHash) {
                 indexToHash.put(index, newHash);
-                String lang = indexToLang.get(index);
-                String text = makeIndexText(index, titles, langToLocale.get(lang), entries);
+                var lang = indexToLang.get(index);
+                var text = makeIndexText(index, titles, langToLocale.get(lang), entries);
                 wb.edit("Indeks:" + index, text, summary);
             }
         }
@@ -141,71 +136,71 @@ public final class AutomatedIndices {
         Files.writeString(hash, new XStream().toXML(indexToHash));
 
         if (!errors.isEmpty()) {
-            String talkPage = wb.getTalkPage(WORKLIST);
-            String text = errors.stream().map(err -> String.format("# %s", err)).collect(Collectors.joining("\n"));
+            var talkPage = wb.getTalkPage(WORKLIST);
+            var text = errors.stream().map(err -> String.format("# %s", err)).collect(Collectors.joining("\n"));
             text = String.format(ERROR_REPORT_TEMPLATE_FMT, String.join("|", MAINTAINERS)) + ":\n" + text + "\n~~~~";
             wb.newSection(talkPage, reader.getPathToDump().getFileName().toString(), text, false, false);
         }
     }
 
     private static void validateEntries(Set<Entry> entries) throws IOException {
-        List<String> languageTemplates = entries.stream()
-            .flatMap(e -> e.languageTemplates.stream())
+        var languageTemplates = entries.stream()
+            .flatMap(e -> e.languageTemplates().stream())
             .distinct()
             .map(t -> String.format("Szablon:%s", t))
             .toList();
 
-        boolean[] existLanguageTemplates = wb.exists(languageTemplates);
-        Set<String> missingLanguageTemplates = new HashSet<>();
+        var existLanguageTemplates = wb.exists(languageTemplates);
+        var missingLanguageTemplates = new HashSet<String>();
 
-        for (int i = 0; i < languageTemplates.size(); i++) {
+        for (var i = 0; i < languageTemplates.size(); i++) {
             if (!existLanguageTemplates[i]) {
                 errors.add(languageTemplates.get(i) + " does not exist");
                 missingLanguageTemplates.add(languageTemplates.get(i));
             }
         }
 
-        entries.stream().map(e -> e.languageTemplates).forEach(list -> list.removeIf(missingLanguageTemplates::contains));
+        entries.stream().map(Entry::languageTemplates).forEach(list -> list.removeIf(missingLanguageTemplates::contains));
 
-        List<String> defTemplates = entries.stream()
-            .flatMap(e -> e.templates.stream())
+        var defTemplates = entries.stream()
+            .flatMap(e -> e.templates().stream())
             .distinct()
             .map(t -> String.format("Szablon:%s", t))
             .toList();
 
-        boolean[] existDefTemplates = wb.exists(defTemplates);
-        Set<String> missingDefTemplates = new HashSet<>();
+        var existDefTemplates = wb.exists(defTemplates);
+        var missingDefTemplates = new HashSet<String>();
 
-        for (int i = 0; i < defTemplates.size(); i++) {
+        for (var i = 0; i < defTemplates.size(); i++) {
             if (!existDefTemplates[i]) {
                 errors.add(defTemplates.get(i) + " does not exist");
                 missingDefTemplates.add(defTemplates.get(i));
             }
         }
 
-        entries.stream().map(e -> e.templates).forEach(list -> list.removeIf(missingDefTemplates::contains));
+        entries.stream().map(Entry::templates).forEach(list -> list.removeIf(missingDefTemplates::contains));
 
-        List<String> categories = entries.stream()
-            .flatMap(e -> e.categories.stream())
+        var categories = entries.stream()
+            .flatMap(e -> e.categories().stream())
             .distinct()
             .map(c -> String.format("Kategoria:%s", c))
             .toList();
 
         if (!categories.isEmpty()) {
-            boolean[] existCategories = wb.exists(categories);
-            Set<String> missingCategories = new HashSet<>();
+            var existCategories = wb.exists(categories);
+            var missingCategories = new HashSet<String>();
 
-            for (int i = 0; i < categories.size(); i++) {
+            for (var i = 0; i < categories.size(); i++) {
                 if (!existCategories[i]) {
                     errors.add(categories.get(i) + " does not exist");
                     missingCategories.add(categories.get(i));
                 }
             }
 
-            entries.stream().map(e -> e.categories).forEach(list -> list.removeIf(missingCategories::contains));
+            entries.stream().map(Entry::categories).forEach(list -> list.removeIf(missingCategories::contains));
         }
 
-        entries.removeIf(e -> e.languageTemplates.isEmpty() || e.templates.isEmpty() || e.categories.isEmpty());
+        entries.removeIf(e -> e.languageTemplates().isEmpty() || e.templates().isEmpty());
     }
 
     private static XMLDumpReader getDumpReader(String[] args) throws IOException {
@@ -221,7 +216,7 @@ public final class AutomatedIndices {
     }
 
     private static ULocale getLocale(String lang) {
-        String code = Stream.of(ULocale.getAvailableLocales())
+        var code = Stream.of(ULocale.getAvailableLocales())
             .filter(locale -> locale.getDisplayLanguage(POLISH_LOCALE).equals(lang))
             .map(ULocale::getISO3Language)
             .distinct()
@@ -237,37 +232,29 @@ public final class AutomatedIndices {
         }
     }
 
-    private static void processDefinitionsField(Field f, Map<String, List<Entry>> langToEntries, ConcurrentMap<String, List<String>> indexToTitles,
-            ConcurrentMap<String, String> indexToLang) {
-        String lang = f.getContainingSection().get().getLang();
-        String title = f.getContainingSection().get().getContainingPage().get().getTitle();
+    private static void processDefinitionsField(Field f, Map<String, List<Entry>> langToEntries, Map<String, List<String>> indexToTitles,
+            Map<String, String> indexToLang) {
+        var lang = f.getContainingSection().get().getLang();
+        var title = f.getContainingSection().get().getContainingPage().get().getTitle();
 
-        for (Entry entry : langToEntries.get(lang)) {
-            if (entry.templates.stream().anyMatch(template -> !ParseUtils.getTemplates(template, f.getContent()).isEmpty())) {
-                String shortLang = stripLanguagePrefix(lang);
-                String index = String.format("%s - %s", StringUtils.capitalize(shortLang), StringUtils.capitalize(entry.indexName));
+        for (var entry : langToEntries.get(lang)) {
+            if (entry.templates().stream().anyMatch(template -> !ParseUtils.getTemplates(template, f.getContent()).isEmpty())) {
+                var shortLang = stripLanguagePrefix(lang);
+                var index = String.format("%s - %s", StringUtils.capitalize(shortLang), StringUtils.capitalize(entry.indexName()));
 
-                indexToTitles.compute(index, (key, value) -> {
-                    if (value == null) {
-                        value = new ArrayList<>();
-                    }
-
-                    value.add(title);
-                    return value;
-                });
-
+                indexToTitles.computeIfAbsent(index, k -> new ArrayList<>()).add(title);
                 indexToLang.putIfAbsent(index, shortLang);
             }
         }
     }
 
     private static String makeIndexText(String index, List<String> titles, ULocale locale, Set<Entry> entries) {
-        final String separator = " - ";
-        String langUpper = index.substring(0, index.indexOf(separator));
-        String langLower = StringUtils.uncapitalize(langUpper);
-        String indexType = index.substring(index.indexOf(separator) + separator.length());
+        final var separator = " - ";
+        var langUpper = index.substring(0, index.indexOf(separator));
+        var langLower = StringUtils.uncapitalize(langUpper);
+        var indexType = index.substring(index.indexOf(separator) + separator.length());
 
-        StringBuilder sb = new StringBuilder();
+        var sb = new StringBuilder();
         sb.append(String.format("{{język linków|%s}}", langLower)).append("{{TOCright}}").append("\n\n");
 
         titles.stream()
@@ -288,21 +275,21 @@ public final class AutomatedIndices {
         sb.append(String.format("[[Kategoria:%s (słowniki tematyczne)|%s]]", langUpper, indexType)).append("\n");
 
         entries.stream()
-            .filter(entry -> entry.indexName.equals(StringUtils.uncapitalize(indexType)))
-            .flatMap(entry -> entry.categories.stream())
+            .filter(entry -> entry.indexName().equals(StringUtils.uncapitalize(indexType)))
+            .flatMap(entry -> entry.categories().stream())
             .forEach(category -> sb.append(String.format("[[Kategoria:%s]]", category)).append("\n"));
 
         return sb.toString().trim();
     }
 
     private static Comparator<Object> makeComparator(ULocale locale) {
-        final Collator collPrimary = Collator.getInstance(locale);
+        final var collPrimary = Collator.getInstance(locale);
         collPrimary.setStrength(Collator.PRIMARY);
 
-        final Collator collSecondary = Collator.getInstance(locale);
+        final var collSecondary = Collator.getInstance(locale);
         collSecondary.setStrength(Collator.SECONDARY);
 
-        final Collator collTertiary = Collator.getInstance(locale);
+        final var collTertiary = Collator.getInstance(locale);
         collTertiary.setStrength(Collator.TERTIARY);
 
         return collPrimary.thenComparing((o1, o2) -> {
@@ -310,17 +297,17 @@ public final class AutomatedIndices {
                 return 0;
             }
 
-            String s1 = (String) o1;
-            String s2 = (String) o2;
+            var s1 = (String)o1;
+            var s2 = (String)o2;
 
-            int secondaryCompare = collSecondary.compare(s1, s2);
+            var secondaryCompare = collSecondary.compare(s1, s2);
 
             if (secondaryCompare == 0) {
                 // same diacritic mark (or lack of), but different case
                 return -collTertiary.compare(s1, s2);
             } else {
-                boolean isLower1 = UCharacter.isLowerCase(s1.codePointAt(0));
-                boolean isLower2 = UCharacter.isLowerCase(s2.codePointAt(0));
+                var isLower1 = UCharacter.isLowerCase(s1.codePointAt(0));
+                var isLower2 = UCharacter.isLowerCase(s2.codePointAt(0));
 
                 if (!(isLower1 ^ isLower2)) {
                     // both are lower or upper case (XNOR)
@@ -333,35 +320,29 @@ public final class AutomatedIndices {
     }
 
     private static String removeDiacriticMarks(String s, ULocale locale) {
-        Collator coll = Collator.getInstance(locale);
+        var coll = Collator.getInstance(locale);
         coll.setStrength(Collator.PRIMARY);
-        String stripped = DIACRITIC_TRANSLITERATOR.transliterate(s);
+        var stripped = DIACRITIC_TRANSLITERATOR.transliterate(s);
         return coll.compare(s, stripped) == 0 ? stripped : s;
     }
 
-    private static final class Entry {
-        String indexName;
-        List<String> templates;
-        List<String> languageTemplates;
-        List<String> categories;
-
+    private record Entry (String indexName, List<String> templates, List<String> languageTemplates, List<String> categories) {
         private static final Pattern SEP = Pattern.compile(",");
 
-        static Entry parseTemplate(String template) {
-            Map<String, String> params = ParseUtils.getTemplateParametersWithValue(template);
+        public static Entry parseTemplate(String template) {
+            var params = ParseUtils.getTemplateParametersWithValue(template);
 
-            Entry entry = new Entry();
-            entry.indexName = params.getOrDefault("nazwa indeksu", "");
-            entry.templates = makeList(SEP.splitAsStream(params.getOrDefault("szablony tematyczne", "")));
-            entry.languageTemplates = makeList(SEP.splitAsStream(params.getOrDefault("szablony języków", "")));
-            entry.categories = makeList(SEP.splitAsStream(params.getOrDefault("kategorie", "")));
+            var indexName = params.getOrDefault("nazwa indeksu", "");
+            var templates = makeList(SEP.splitAsStream(params.getOrDefault("szablony tematyczne", "")));
+            var languageTemplates = makeList(SEP.splitAsStream(params.getOrDefault("szablony języków", "")));
+            var categories = makeList(SEP.splitAsStream(params.getOrDefault("kategorie", "")));
 
-            if (entry.indexName.isEmpty() || entry.templates.isEmpty() || entry.languageTemplates.isEmpty()) {
+            if (indexName.isEmpty() || templates.isEmpty() || languageTemplates.isEmpty()) {
                 errors.add("Illegal parameters to template " + template.replace("\n", ""));
                 return null;
             }
 
-            return entry;
+            return new Entry(indexName, templates, languageTemplates, categories);
         }
 
         private static List<String> makeList(Stream<String> stream) {
@@ -369,30 +350,8 @@ public final class AutomatedIndices {
         }
 
         @Override
-        public String toString() {
-            StringBuilder sb = new StringBuilder();
-            sb.append("[indexName=").append(indexName);
-            sb.append(",templates=").append(templates);
-            sb.append(",languageTemplates=").append(languageTemplates);
-            sb.append(",categories=").append(categories);
-            sb.append("]");
-            return sb.toString();
-        }
-
-        @Override
         public int hashCode() {
             return indexName.hashCode();
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (obj == this) {
-                return true;
-            } else if (obj instanceof Entry e) {
-                return indexName.equals(e.indexName);
-            } else {
-                return false;
-            }
         }
     }
 }
