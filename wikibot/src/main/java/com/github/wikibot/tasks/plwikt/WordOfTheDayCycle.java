8ae0413a8c62da9e3f164e8f39076ccb0f99cb52
Bartek Łukawski <bwmn.peter@gmail.com>
2022-06-24 16:35:27 +0200

    Use double quotes instead of an apostrophe

--- wikibot/src/main/java/com/github/wikibot/tasks/plwikt/WordOfTheDayCycle.java
+++ wikibot/src/main/java/com/github/wikibot/tasks/plwikt/WordOfTheDayCycle.java
@@ -13,11 +13,11 @@ public class WordOfTheDayCycle {
     public static void main(String[] args) throws Exception {
         var page = wb.getContentOfPages(List.of(TARGET)).get(0);
 
-        var current = Pattern.compile("aktualne *+= *+'(.+?)'").matcher(page.getText()).results()
+        var current = Pattern.compile("aktualne *+= *+\"(.+?)\"").matcher(page.getText()).results()
             .map(mr -> mr.group(1))
             .findAny().get();
 
-        var newText = Pattern.compile("poprzednie *+= *+'(.+?)'").matcher(page.getText())
+        var newText = Pattern.compile("poprzednie *+= *+\"(.+?)\"").matcher(page.getText())
             .replaceFirst(mr -> mr.group().replace(mr.group(1), current));
 
         Login.login(wb);
