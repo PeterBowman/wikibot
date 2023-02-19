96b15de29eae44de82384143ff199d9126a9ef1d
Bartek Łukawski <bwmn.peter@gmail.com>
2023-02-19 20:08:06 +0100

    Enhance LatinInfinitiveBacklinks task

--- wikibot/src/main/java/com/github/wikibot/tasks/plwikt/LatinInfinitiveBacklinks.java
+++ wikibot/src/main/java/com/github/wikibot/tasks/plwikt/LatinInfinitiveBacklinks.java
@@ -4,13 +4,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.text.Collator;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
+import java.util.Locale;
+import java.util.Objects;
 import java.util.Optional;
-import java.util.Set;
-import java.util.TreeMap;
-import java.util.TreeSet;
 import java.util.function.Function;
+import java.util.regex.MatchResult;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -26,25 +28,37 @@ import com.github.wikibot.dumps.XMLDumpConfig;
 import com.github.wikibot.dumps.XMLDumpTypes;
 import com.github.wikibot.dumps.XMLRevision;
 import com.github.wikibot.main.Wikibot;
+import com.github.wikibot.parsing.plwikt.Field;
+import com.github.wikibot.parsing.plwikt.FieldTypes;
+import com.github.wikibot.parsing.plwikt.Page;
 import com.github.wikibot.utils.Login;
 
 public class LatinInfinitiveBacklinks {
     private static final Path LOCATION = Paths.get("./data/tasks.plwikt/LatinInfinitiveBacklinks/");
-    private static final String TARGET_PAGE = "Wikisłownikarz:PBbot/łacińskie bezokoliczniki w etymologii";
+    private static final String TARGET_PAGE = "Wikisłownikarz:PBbot/łacińskie bezokoliczniki";
+    private static final Pattern P_LINK = Pattern.compile("\\[\\[:?([^\\]|]+)(?:\\|((?:]?[^\\]|])*+))*\\]\\]");
 
-    private static final List<String> LATIN_SHORTS = List.of("łac", "łaciński", "łacińskie", "łacina", "stłac");
-    private static final List<String> LATIN_DESINENCES = List.of("are", "āre", "ere", "ēre", "ĕre", "ire", "īre");
+    private static final String[] LATIN_SHORTS = {"łac", "łaciński", "łacińskie", "łacina", "stłac"};
+    private static final String[] LATIN_DESINENCES = {"are", "āre", "ere", "ēre", "ĕre", "ire", "īre"};
+
+    private static final Wikibot wb = Wikibot.newSession("pl.wiktionary.org");
 
     private static final String INTRO;
 
     static {
         INTRO = """
-            Prawdopodobne wystąpienia łacińskich bezokoliczników w wywołaniach szablonów etymologii
-            ({{s|etym}}, {{s|etym2}}, {{s|etymn}} oraz {{s|etymn2}}):
-            [[Specjalna:Niezmienny link/7662696#Łacińskie bezokoliczniki|dyskusja]], [[/mapowania|mapowania]].
-
-            Dane na podstawie %s. Aktualizacja: ~~~~~.
+            Prawdopodobne wystąpienia łacińskich bezokoliczników ([[Specjalna:Permalink/8077565#Łacińskie bezokoliczniki|dyskusja]], [[/mapowania]]).
+            __TOC__
+            Dane na podstawie %s.
             ----
+            === Szablony etymologii ===
+            %s
+
+            === Tłumaczenia w hasłach polskich ===
+            %s
+
+            === Wikilinki w hasłach łacińskich ===
+            %s
             """;
     }
 
@@ -57,40 +71,35 @@ public class LatinInfinitiveBacklinks {
             return;
         }
 
-        var wb = Wikibot.newSession("pl.wiktionary.org");
+        var dump = optDump.get();
+
         Login.login(wb);
 
-        var dump = optDump.get();
-        var occurrences = new TreeMap<String, Set<String>>();
+        var etymResults = doEtymology(dump);
+        System.out.println("Etymology results: " + etymResults.size());
 
-        try (var stream = dump.stream()) {
-            stream
-                .filter(XMLRevision::isMainNamespace)
-                .filter(XMLRevision::nonRedirect)
-                .forEach(rev -> mapOccurrences(rev.getTitle(), rev.getText(), occurrences));
-        }
+        var translationResults = doTranslations(dump);
+        System.out.println("Translation results: " + translationResults.size());
 
-        System.out.printf("Got %d unfiltered occurrences.%n", occurrences.size());
+        var wikilinkResults = doWikilinks(dump);
+        System.out.println("Wikilink results: " + wikilinkResults.size());
 
-        var titles = occurrences.keySet().stream().toList();
-        occurrences.clear();
+        var hashPath = LOCATION.resolve("hash.txt");
+        var computedHash = Objects.hash(etymResults, translationResults, wikilinkResults);
 
-        wb.getContentOfPages(titles).stream().forEach(pc -> mapOccurrences(pc.getTitle(), pc.getText(), occurrences));
-        System.out.printf("Got %d filtered occurrences.%n", occurrences.size());
+        if (!Files.exists(hashPath) || Integer.parseInt(Files.readString(hashPath)) != computedHash) {
+            Files.write(LOCATION.resolve("etymology.txt"), etymResults);
+            Files.write(LOCATION.resolve("translations.txt"), translationResults);
+            Files.write(LOCATION.resolve("wikilinks.txt"), wikilinkResults);
 
-        var hash = LOCATION.resolve("hash.txt");
+            var text = String.format(INTRO, dump.getDescriptiveFilename(),
+                                     String.join("\n", etymResults),
+                                     String.join("\n", translationResults),
+                                     String.join("\n", wikilinkResults));
 
-        if (!Files.exists(hash) || Integer.parseInt(Files.readString(hash)) != occurrences.hashCode()) {
-            var list = occurrences.entrySet().stream()
-                .map(e -> String.format("#[[%s]]: %s", e.getKey(), e.getValue().stream().collect(Collectors.joining(", "))))
-                .toList();
-
-            Files.write(LOCATION.resolve("results.txt"), list);
-
-            var text = String.format(INTRO, dump.getDescriptiveFilename()) + String.join("\n", list);
             wb.edit(TARGET_PAGE, text, "aktualizacja", false, false, -2, null);
 
-            Files.writeString(hash, Integer.toString(occurrences.hashCode()));
+            Files.writeString(hashPath, Integer.toString(computedHash));
         } else {
             System.out.println("No changes detected.");
         }
@@ -125,20 +134,106 @@ public class LatinInfinitiveBacklinks {
         return dumpConfig.fetch();
     }
 
-    private static void mapOccurrences(String title, String text, Map<String, Set<String>> occurrences) {
-        Stream.of(
-                ParseUtils.getTemplates("etym", text).stream(),
-                ParseUtils.getTemplates("etymn", text).stream(),
-                ParseUtils.getTemplates("etym2", text).stream(),
-                ParseUtils.getTemplates("etymn2", text).stream()
-            )
-            .flatMap(Function.identity())
-            .map(ParseUtils::getTemplateParametersWithValue)
-            .filter(params -> StringUtils.equalsAny(params.getOrDefault("ParamWithoutName1", ""), LATIN_SHORTS.toArray(new String[0])))
-            .filter(params -> params.getOrDefault("ParamWithoutName2", "").length() > 3)
-            .map(params -> params.get("ParamWithoutName2"))
-            .filter(param -> !param.contains("-"))
-            .filter(param -> StringUtils.endsWithAny(param, LATIN_DESINENCES.toArray(new String[0])))
-            .forEach(param -> occurrences.computeIfAbsent(title, item -> new TreeSet<>()).add(param));
+    private static List<String> doEtymology(XMLDump dump) throws IOException {
+        try (var stream = dump.stream()) {
+            return stream
+                .filter(XMLRevision::isMainNamespace)
+                .filter(XMLRevision::nonRedirect)
+                .<Item>mapMulti((rev, cons) -> {
+                    var infinitives = Stream.of(
+                            ParseUtils.getTemplates("etym", rev.getText()).stream(),
+                            ParseUtils.getTemplates("etymn", rev.getText()).stream(),
+                            ParseUtils.getTemplates("etym2", rev.getText()).stream(),
+                            ParseUtils.getTemplates("etymn2", rev.getText()).stream()
+                        )
+                        .flatMap(Function.identity())
+                        .map(ParseUtils::getTemplateParametersWithValue)
+                        .filter(params -> StringUtils.equalsAny(params.getOrDefault("ParamWithoutName1", ""), LATIN_SHORTS))
+                        .filter(params -> params.getOrDefault("ParamWithoutName2", "").length() > 3)
+                        .map(params -> params.get("ParamWithoutName2"))
+                        .filter(param -> !param.contains("-"))
+                        .filter(param -> StringUtils.endsWithAny(param, LATIN_DESINENCES))
+                        .sorted()
+                        .distinct()
+                        .toList();
+
+                    if (!infinitives.isEmpty()) {
+                        cons.accept(new Item(rev.getTitle(), infinitives));
+                    }
+                })
+                .sorted(Comparator.comparing(Item::title))
+                .map(i -> String.format("#[[%s]]: %s", i.title(), i.infinitives().stream().collect(Collectors.joining(", "))))
+                .toList();
+        }
     }
+
+    private static List<String> doTranslations(XMLDump dump) {
+        var patt = Pattern.compile("^\\* *łaciński:.+", Pattern.MULTILINE);
+
+        try (var stream = dump.stream()) {
+            return stream
+                .filter(XMLRevision::isMainNamespace)
+                .filter(XMLRevision::nonRedirect)
+                .map(Page::wrap)
+                .flatMap(p -> p.getPolishSection().stream())
+                .flatMap(s -> s.getField(FieldTypes.TRANSLATIONS).stream())
+                .filter(f -> f.getContent().contains("łaciński"))
+                .<Item>mapMulti((f, cons) -> {
+                    var infinitives = patt.matcher(f.getContent()).results()
+                        .map(MatchResult::group)
+                        .flatMap(line -> P_LINK.matcher(line).results())
+                        .map(mr -> mr.group(1))
+                        .map(String::strip)
+                        .filter(title -> StringUtils.endsWithAny(title, LATIN_DESINENCES))
+                        .sorted()
+                        .distinct()
+                        .toList();
+
+                    if (!infinitives.isEmpty()) {
+                        var title = f.getContainingSection().get().getContainingPage().get().getTitle();
+                        cons.accept(new Item(title, infinitives));
+                    }
+                })
+                .sorted(Comparator.comparing(Item::title, Collator.getInstance(new Locale("pl"))))
+                .map(i -> String.format("#[[%s]]: %s", i.title(), String.join(", ", i.infinitives())))
+                .toList();
+        }
+    }
+
+    private static List<String> doWikilinks(XMLDump dump) throws IOException {
+        var patt = Pattern.compile("→[^•]+•?");
+
+        try (var stream = dump.stream()) {
+            return stream
+                .filter(XMLRevision::isMainNamespace)
+                .filter(XMLRevision::nonRedirect)
+                .map(Page::wrap)
+                .flatMap(p -> p.getSection("łaciński", true).stream())
+                .<Item>mapMulti((s, cons) -> {
+                    var infinitives = s.getAllFields().stream()
+                        .filter(f -> f.getFieldType() != FieldTypes.INFLECTION)
+                        .filter(f -> !f.isEmpty())
+                        .map(Field::getContent)
+                        .map(content -> patt.matcher(content).replaceAll(""))
+                        .map(content -> s.getHeader() + content) // also analyze links in header
+                        .flatMap(content -> P_LINK.matcher(content).results())
+                        .map(mr -> mr.group(1))
+                        .map(String::strip)
+                        .filter(title -> StringUtils.endsWithAny(title, LATIN_DESINENCES))
+                        .sorted()
+                        .distinct()
+                        .toList();
+
+                    if (!infinitives.isEmpty()) {
+                        var title = s.getContainingPage().get().getTitle();
+                        cons.accept(new Item(title, infinitives));
+                    }
+                })
+                .sorted(Comparator.comparing(Item::title))
+                .map(i -> String.format("#[[%s]]: %s", i.title(), String.join(", ", i.infinitives())))
+                .toList();
+        }
+    }
+
+    record Item(String title, List<String> infinitives) {}
 }
