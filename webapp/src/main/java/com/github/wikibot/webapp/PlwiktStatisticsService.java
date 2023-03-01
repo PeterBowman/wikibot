d0c7c374c605b979aa002bb531dd630f8298c4f9
Bartek Łukawski <bwmn.peter@gmail.com>
2023-03-02 00:55:25 +0100

    Create plwikt stats webservice

---
+++ webapp/src/main/java/com/github/wikibot/webapp/PlwiktStatisticsService.java
@@ -0,0 +1,121 @@
+package com.github.wikibot.webapp;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Map;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.servlet.ServletException;
+import javax.servlet.UnavailableException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+import org.json.JSONObject;
+
+public class PlwiktStatisticsService extends HttpServlet {
+    private static final String TARGET_CATEGORY = "Indeks słów wg języków";
+    private static final int EXPIRY_TIME_MSECS = 1000 * 60 * 5; // 5 minutes
+
+    private int cachedResult;
+    private long lastUpdate;
+    private boolean isRunning;
+
+    private DataSource dataSource;
+
+    @Override
+    public void init() throws ServletException {
+        try {
+            var context = (Context) new InitialContext().lookup("java:comp/env");
+            dataSource = (DataSource) context.lookup("jdbc/plwiktionary-web");
+        } catch (NamingException e) {
+            throw new UnavailableException(e.getMessage());
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+
+        var now = System.currentTimeMillis();
+
+        int localCachedResult;
+        long localLastUpdate;
+
+        final boolean shouldReturnCached;
+
+        synchronized (this) {
+            localCachedResult = cachedResult;
+            localLastUpdate = lastUpdate;
+
+            if (now - lastUpdate < EXPIRY_TIME_MSECS || isRunning) {
+                shouldReturnCached = true;
+            } else {
+                shouldReturnCached = false;
+                isRunning = true;
+            }
+        }
+
+        if (shouldReturnCached) {
+            response.getWriter().print(makeOutput(localCachedResult, true, localLastUpdate));
+            return;
+        }
+
+        // only one thread can reach this point concurrently
+
+        try {
+            localCachedResult = getStats();
+            localLastUpdate = System.currentTimeMillis();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        synchronized (this) {
+            cachedResult = localCachedResult;
+            lastUpdate = localLastUpdate;
+            isRunning = false;
+        }
+
+        response.getWriter().print(makeOutput(localCachedResult, false, localLastUpdate));
+    }
+
+    private int getStats() throws SQLException {
+        try (var connection = dataSource.getConnection()) {
+            var query = String.format("""
+                SELECT
+                    SUM(cat_pages - cat_subcats - cat_files) AS total_entries
+                FROM page
+                    INNER JOIN categorylinks ON cl_from = page_id
+                    INNER JOIN category ON cat_title = page_title
+                WHERE
+                    cl_to = '%s'
+                    AND page_namespace = 14;
+                """, TARGET_CATEGORY.replace(' ', '_'));
+
+            var statement = connection.createStatement();
+            var resultSet = statement.executeQuery(query);
+
+            if (resultSet.next()) {
+                return resultSet.getInt("total_entries");
+            }
+        }
+
+        throw new RuntimeException("no results");
+    }
+
+    private JSONObject makeOutput(int value, boolean isCached,long timestamp) {
+        return new JSONObject(Map.of(
+            "canonical", value,
+            "cached", isCached,
+            "timestamp", timestamp
+        ));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+}
